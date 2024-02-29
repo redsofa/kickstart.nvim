830a46731a9fde4cd6fb2051fb1b0fcd1e9c6892
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2024-02-29 01:52:36 -0400

    checkpoint

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,22 +1,18 @@
-vim.opt.colorcolumn = "80"
-vim.api.nvim_create_user_command(
-  "Cheats",
-  "e ~/.config/nvim/lua/config/Cheats.txt",
-  {}
-) 
+vim.opt.colorcolumn = '80'
+vim.cmd.colorscheme 'catppuccin-frappe'
+vim.api.nvim_create_user_command('Cheats', 'e ~/.config/nvim/lua/config/Cheats.txt', {})
 --[[
   Source : https://www.reddit.com/r/neovim/comments/zv91wz/range_formatting/  
   Range format any block of code using the built-in format function 
 --]]
 local range_formatting = function()
-    local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, "<"))
-    local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, ">"))
-    vim.lsp.buf.format({
-        range = {
-            ["start"] = { start_row, 0 },
-            ["end"] = { end_row, 0 },
-        },
-        async = true,
-    })
+  local start_row, _ = unpack(vim.api.nvim_buf_get_mark(0, '<'))
+  local end_row, _ = unpack(vim.api.nvim_buf_get_mark(0, '>'))
+  vim.lsp.buf.format {
+    range = {
+      ['start'] = { start_row, 0 },
+      ['end'] = { end_row, 0 },
+    },
+    async = true,
+  }
 end
-vim.keymap.set("v", "<leader>f", range_formatting, { desc = "Range Formatting"})
