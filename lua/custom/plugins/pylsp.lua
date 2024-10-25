3b81b4ffa0e2c23d1a6ad5bd9a56b789d811f9d7
Rene Richard <rene.richard@nrc-cnrc.gc.ca>
2024-10-25 02:10:27 +0000

    pylsp config

---
+++ lua/custom/plugins/pylsp.lua
@@ -0,0 +1,14 @@
+return {
+  require('lspconfig').pylsp.setup {
+    settings = {
+      pylsp = {
+        plugins = {
+          pycodestyle = {
+            ignore = { 'W391' },
+            maxLineLength = 100,
+          },
+        },
+      },
+    },
+  },
+}
